f491d9aa96c50e643cc872242937f6975c6ba07f
Mungert69 <contact@mahadeva.co.uk>
2025-07-24 00:31:29 +0100

    added             "execute_query",

--- Objects/Factory/AccountTypeFactory.cs
+++ Objects/Factory/AccountTypeFactory.cs
@@ -212,6 +212,7 @@ public class AccountTypeFactory
             "validate_quantum_config",
             "call_security_basic_flow",
             "call_cmd_processor_builder_flow",
+            "execute_query",
             "cp_*"
         },
             "Standard" => new List<string>
@@ -247,6 +248,7 @@ public class AccountTypeFactory
             "validate_quantum_config",
             "call_security_basic_flow",
             "call_cmd_processor_builder_flow",
+            "execute_query",
             "cp_*"
         },
             "Professional" => new List<string>
@@ -286,6 +288,7 @@ public class AccountTypeFactory
             "validate_quantum_config",
             "call_security_basic_flow",
             "call_cmd_processor_builder_flow",
+            "execute_query",
             "cp_*"
         },
             "Enterprise" => new List<string>
@@ -328,6 +331,7 @@ public class AccountTypeFactory
             "validate_quantum_config",
             "call_security_basic_flow",
             "call_cmd_processor_builder_flow",
+            "execute_query",
             "cp_*"
         },
             "God" => new List<string>
@@ -370,6 +374,7 @@ public class AccountTypeFactory
             "validate_quantum_config",
             "call_security_basic_flow",
             "call_cmd_processor_builder_flow",
+            "execute_query",
             "cp_*"
         },
             _ => new List<string>
