8cce29838d445498afaab49370a5fa3ce7c86bf0
Mungert69 <contact@mahadeva.co.uk>
2025-07-29 22:38:47 +0100

    added json properties

--- Objects/BlogQNA.cs
+++ Objects/BlogQNA.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 
 namespace NetworkMonitor.Objects;
 
@@ -10,8 +12,13 @@ public class BlogQNA
 {
     [Key]
     public int Id { get; set; }
+
+    [JsonPropertyName("question")]
     public string Question { get; set; } = "";
+
+     [JsonPropertyName("answer")]
     public string Answer { get; set; } = "";
+
     public virtual int BlogID { get; set; } = 0;
 
 }
