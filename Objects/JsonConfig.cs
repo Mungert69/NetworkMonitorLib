a0ea80ef2a19fd9ab6c746fc6e9779b71265a538
Mungert69 <contact@mahadeva.co.uk>
2026-09-07 18:06:02 +0000

    use authkeys

--- Objects/JsonConfig.cs
+++ Objects/JsonConfig.cs
@@ -132,7 +132,6 @@ namespace NetworkMonitor.Objects
     [JsonSerializable(typeof(List<ProductObj>))]
     [JsonSerializable(typeof(ProductObj))]
     [JsonSerializable(typeof(ProcessorInitObj))]
-    [JsonSerializable(typeof(RefreshAuthKeyObj))]
     [JsonSerializable(typeof(ProcessorDataBuilder))]
     [JsonSerializable(typeof(ProcessorUserEventObj))]
     [JsonSerializable(typeof(ProcessorConnectObj))]
--- Objects/ServiceMessage/RefreshAuthKeyObj.cs
+++
@@ -1,9 +0,0 @@
-namespace NetworkMonitor.Objects.ServiceMessage
-{
-    public class RefreshAuthKeyObj
-    {
-        public bool RefreshUserAgent { get; set; }
-        public bool RefreshSystemAgent { get; set; }
-        public string Reason { get; set; } = string.Empty;
-    }
-}
